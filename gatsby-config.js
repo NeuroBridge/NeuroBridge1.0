a1b7c108ae00f6c28515bac05e9a8163836c91ed
Suejin <kim.suejin.20@gmail.com>
2022-02-14 15:43:56 -0500

    add gatsby material ui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,9 @@ module.exports = {
         icon: `src/images/construction-favicon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-material-ui`,
+   
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
