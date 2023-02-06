38451eaf811030a830aa3785aed7ab37334af46b
suejinkim20 <kim.suejin.20@gmail.com>
2023-02-06 11:23:07 -0500

    add published papers to publications.js

--- src/pages/publication.js
+++ src/pages/publication.js
@@ -2,12 +2,13 @@ import * as React from "react"
 import { Link } from '../components/link'
 import Seo from "../components/layout/seo"
 import featureImg from '../images/features-5.png'
-import { useWebinars } from '../hooks'
+import { usePublications } from '../hooks'
 import BreadcrumbNav from '../components/layout/breadcrumbs'
 import { Container, Typography, Box } from '@mui/material'
 
 const PublicationPage = () => {
-  const webinars = useWebinars()
+  const webinars = usePublications()[0]
+  const publications= usePublications()[1]
   const styles = {
     sectionTitle: {
         },
@@ -61,16 +62,14 @@ const PublicationPage = () => {
         <Typography variant='h1'>Neurobridge Publications</Typography>
         <Box sx={styles.container}>
           <Box sx={styles.publicationType}>
-            <Typography variant='h2'>Webinars/Seminars</Typography>
+            <Typography variant='h2'>Published Papers</Typography>
           </Box>
           <Box sx={styles.publicationList}>
-            <Typography variant='h2'>Recent Webinars/Seminars</Typography>
-            <Typography variant='h3'>List of Webinars/Seminars</Typography>
             <ul>
-              {webinars.map((item)=> (
+              {publications.map((item)=> (
                 <li key={item.title} sx={styles.publicationItem}>
-                  <Typography paragraph>{item.description}</Typography>
-                  <Link to={item.slides}>Click here to access the slides</Link>
+                  <Typography paragraph>{item.citation}</Typography>
+                  <Link to={item.link}>Click here to access the slides</Link>
                 </li>
               ))}
             </ul>
@@ -80,7 +79,21 @@ const PublicationPage = () => {
             src={featureImg}
             alt=''
             sx={styles.featureImage}
-          >
+          />
+        </Box>
+        <Box sx={styles.container}>
+          <Box sx={styles.publicationType}>
+            <Typography variant='h2'>Webinars/Seminars</Typography>
+          </Box>
+          <Box sx={styles.publicationList}>
+            <ul>
+              {webinars.map((item)=> (
+                <li key={item.title} sx={styles.publicationItem}>
+                  <Typography paragraph>{item.description}</Typography>
+                  <Link to={item.slides}>Click here to access the slides</Link>
+                </li>
+              ))}
+            </ul>
           </Box>
         </Box>
       </Container>
