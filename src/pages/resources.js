2236da8ae700a80204913bfd71a1630b82c428db
suejinkim20 <kim.suejin.20@gmail.com>
2023-04-26 01:00:52 -0400

    update resources page styles

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -3,123 +3,86 @@ import { Link } from '../components/link'
 import Seo from '../components/layout/seo'
 import featureImg from '../images/features-1.png'
 import BreadcrumbNav from '../components/layout/breadcrumbs'
-import { Box, Link as MuiLink, Container, Typography } from "@mui/material"
+import { Box, Link as MuiLink, Container, Typography, Stack, Button } from "@mui/material"
 import { useTheme } from '@mui/material/styles'
 
 const resources = [
   {
     source: 'Github',
     title: 'Github Repo' ,
-    subheading: 'Visit our Github repository to access the latest version of the software release.',
-    text: 'Click below to access the Github page. It has annotation project, ontology and neuro-bridge projects.',
+    subheading: '',
+    text: 'Visit our Github repository to access the latest version of the software release. The repository contains annotation project, ontology, and neuro-bridge projets. Click below to access.',
     resourceLink: 'https://github.com/NeuroBridge',
   },
   {
     source: 'Github',
     title: 'Ontology' ,
-    subheading: 'Click below to directly access the current ontology.',
-    text: '',
+    subheading: '',
+    text: 'Click below to directly access the current ontology.',
     resourceLink: 'https://github.com/NeuroBridge/neuro-ontologies/blob/main/neurobridge/NeuroBridge_032423.owl',
   },
 ]
 
 const ResourcesPage = () => {
-    const theme = useTheme()
-    const styles = {
-        contentContainer: {
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            paddingTop: '30px',
-        },
-        resourceContainer: {
-            display: 'flex',
-            paddingBottom: '30px',
-        },
-        resourceName: {
-            width: '25%',
-            display: 'flex',
-            justifyContent: 'space-between',
-            marginRight: '40px',
-            '& a': {
-                textDecoration: 'none',
-                color: theme.palette.text.secondary,
-                fontWeight: '600',
-                fontSize: '1.2rem',
-            },
-            '&:after': {
-                content: '""',
-                background: 'red',
-                height: '35px',
-                width: '3px',
-            }
-        },
-        resourceContent: {
-            display: 'flex',
-        },
-        resourceDescription: {
-            '& h2': {
-                paddingBottom: '15px',
-            },
-            '& h3': {
-                paddingBottom: '10px',
-                fontStyle: 'italic',
-                color: 'grey',
-            },
-            '& a': {
-                textDecoration: 'none',
-                color: theme.palette.text.secondary,
-                fontWeight: '600',
-                textTransform: 'uppercase',
-                letterSpacing: '0.7px',
-            }
-        },
-        image: {
-            padding: '20px',
-            height: '250px',
-        },
-    }
+  const theme = useTheme()
 
-    const breadcrumbs = [
-        { text: 'Home', path: '/' },
-        { text: 'Resources', path: '/resources' },
-    ]
+  const styles = {
+    section: {
+      display: 'flex',
+      paddingBottom: '2rem'
+    },
+    sectionTitle: {
+      flex: 1,
+      minWidth: '200px',
+    },
+    sectionContent: {
+      flex: 4
+    },
+  }
 
-    return (
-        <>
-            <Seo title="NeuroBridge Resources" />
-            <BreadcrumbNav crumbs={breadcrumbs} title='Resources'/>
-            <Container >
-                <Typography variant='h1'>Our Latest Updated Software</Typography>
-                <Box sx={styles.contentContainer}>
-                    {software.map((item) => (
-                        <Box key={item.name} sx={styles.resourceContainer}>
-                            <Box sx={styles.resourceName}>
-                                <MuiLink component={Link} to={item.resourceLink} sx={styles.resourceLinkLeft}>Github</MuiLink>
-                            </Box>
-                            <Box sx={styles.resourceContent}>
-                                <Box sx={styles.resourceDescription}>
-                                    <Typography variant='h2'>{item.title}</Typography>
-                                    <Typography variant='h3'>{item.subheading}</Typography>
-                                    <Typography paragraph>{item.text}</Typography>
-                                    <MuiLink component={Link} to={item.resourceLink}>Github</MuiLink>
-                                </Box>
-                                <Box
-                                    component='img'
-                                    key={item.name}
-                                    src={item.photo}
-                                    alt={item.name}
-                                    sx={styles.image}
-                                />
-                            </Box>
+  const breadcrumbs = [
+    { text: 'Home', path: '/' },
+    { text: 'Resources', path: '/resources' },
+  ]
 
-                        </Box>
+  return (
+    <>
+      <Seo title="NeuroBridge Resources" />
+      <BreadcrumbNav crumbs={breadcrumbs} title='Resources'/>
+      <Container>
+        <Typography variant='h1'>Our Latest Updated Software</Typography>
+          {
+          resources.map((item)=> (
+            <Stack
+              key={item.title}
+              direction={{ xs: 'column', md: 'row' }}
+              spacing={{ xs: 1, md: 2 }}
+              sx={styles.section}
+            >
+              <Box sx={styles.sectionTitle}>
+                <Typography variant='h2'>{item.title}</Typography>
+              </Box>
 
-                    ))}
-                </Box>
-            </Container>
-        </>
-    )
+              <Box sx={styles.sectionContent}>
+                <Typography variant='h3'>{item.subheading}</Typography>
+                <Typography>{item.text}</Typography>
+                {/* <Link to={item.resourceLink}>{item.source}</Link> */}
+                <Button 
+                  // component={Link} 
+                  href={item.resourceLink}
+                  variant="contained"
+                  color='secondary'
+                  sx={{marginTop: '1rem'}}
+                >
+                  {item.source}
+                </Button>
+              </Box>
+            </Stack>
+          ))
+        }
+      </Container>
+    </>
+  )
 }
 
 export default ResourcesPage
